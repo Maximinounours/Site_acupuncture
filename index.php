3c06ba15baae1b505743df043aa23a4e9ecc9ea1
Maxime Leriche <maxime.leriche@cpe.fr>
2021-02-24 17:28:26 +0100

    php

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (!isset($_GET["page"])) { $page = ""; }else{ $page = $_GET["page"]; } // contrôle de $_GET["page"]
-// et assignation d'une valeur à $page include("moteur.php"); // inclusion de moteur.php
-echo template($page); // appel à la fonction template() et affichage du résultat
-?>
