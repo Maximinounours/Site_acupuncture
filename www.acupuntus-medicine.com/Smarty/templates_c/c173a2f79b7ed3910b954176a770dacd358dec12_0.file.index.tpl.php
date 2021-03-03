65bc92c1f6ebf6e5fea7e57585814c82bdfed838
Maxime <maxime.leriche@cpe.fr>
2021-03-03 14:55:46 +0100

    acupunctus dossier

---
+++ www.acupuntus-medicine.com/Smarty/templates_c/c173a2f79b7ed3910b954176a770dacd358dec12_0.file.index.tpl.php
@@ -0,0 +1,34 @@
+<?php
+/* Smarty version 3.1.39, created on 2021-02-25 18:36:17
+  from '/var/www/html/www.exemple.com/Smarty/templates/index.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_6037e01118d890_33274471',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'c173a2f79b7ed3910b954176a770dacd358dec12' => 
+    array (
+      0 => '/var/www/html/www.exemple.com/Smarty/templates/index.tpl',
+      1 => 1614273904,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6037e01118d890_33274471 (Smarty_Internal_Template $_smarty_tpl) {
+?><html>
+  <head>
+    <title>Smarty</title>
+  </head>
+  <body>
+    Hello, <?php echo $_smarty_tpl->tpl_vars['name']->value;?>
+!
+  </body>
+</html>
+<?php }
+}
